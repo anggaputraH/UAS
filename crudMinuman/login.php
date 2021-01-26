6ae137c64b85db3ff9826b451137db273b6e90bd
anggaputraH <78005087+anggaputraH@users.noreply.github.com>
2021-01-26 10:15:37 +0700

    Create login.php

---
+++ crudMinuman/login.php
@@ -0,0 +1,65 @@
+<?php session_start(); ?>
+<html>
+<head>
+	<title>Login</title>
+</head>
+
+<body>
+<a href="index.php">Home</a> <br />
+<?php
+include("koneksi.php");
+
+if(isset($_POST['submit'])) {
+	$user = mysqli_real_escape_string($mysqli, $_POST['username']);
+	$pass = mysqli_real_escape_string($mysqli, $_POST['password']);
+
+	if($user == "" || $pass == "") {
+		echo "Username dan Password kosong!";
+		echo "<br/>";
+		echo "<a href='login.php'>Go back</a>";
+	} else {
+		$result = mysqli_query($mysqli, "SELECT * FROM login WHERE username='$user' AND password='$pass' ")
+					or die("Could not execute the select query.");
+		
+		$row = mysqli_fetch_assoc($result);
+		
+		if(is_array($row) && !empty($row)) {
+			$validuser = $row['username'];
+			$_SESSION['valid'] = $validuser;
+			$_SESSION['name'] = $row['name'];
+			$_SESSION['id'] = $row['id'];
+			$_SESSION['role'] = $row['role'];
+		} else {
+			echo "Username dan password salah";
+			echo "<br/>";
+			echo "<a href='login.php'>Go back</a>";
+		}
+
+		if(isset($_SESSION['valid'])) {
+			header('Location: index.php');			
+		}
+	}
+} else {
+?>
+	<p><font size="+2">Login</font></p>
+	<form name="form1" method="post" action="">
+		<table width="75%" border="0">
+			<tr> 
+				<td width="10%">Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr> 
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr> 
+				<td>&nbsp;</td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+<?php
+}
+?>
+</body>
+</html>
